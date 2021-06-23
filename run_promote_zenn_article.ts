6d24b53e5b1fd1131b185c8f5271bce0cf16c4cc
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-23 18:33:01 +0900

    add console.log for github actions

--- run_promote_zenn_article.ts
+++ run_promote_zenn_article.ts
@@ -1,7 +1,8 @@
-import { IFTTT_WEBHOOK_KEY } from "./env.ts";
+import { IFTTT_WEBHOOK_KEY ,LINE_ACCESS_TOKEN} from "./env.ts";
 import { sendTweet } from "./tweet_with_ifttt.ts";
 import { genPromotion } from "./promote_zenn_article.ts";
 
 const message = await genPromotion("kawarimidoll");
+console.log(LINE_ACCESS_TOKEN);
 console.log({ message });
 console.log(await sendTweet({ message, key: IFTTT_WEBHOOK_KEY }));
