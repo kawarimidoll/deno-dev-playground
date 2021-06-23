61c2e9817559a2001941e9ff25971dac505a966a
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-23 14:04:09 +0900

    fix runner

--- run_promote_zenn_article.ts
+++ run_promote_zenn_article.ts
@@ -1,8 +1,7 @@
 import { IFTTT_WEBHOOK_KEY } from "./env.ts";
-import { Logger } from "./logger.ts";
 import { sendTweet } from "./tweet_with_ifttt.ts";
 import { genPromotion } from "./promote_zenn_article.ts";
 
 const message = await genPromotion("kawarimidoll");
-Logger.info({ message });
-Logger.info(await sendTweet({ message, key: IFTTT_WEBHOOK_KEY }));
+console.log({ message });
+console.log(await sendTweet({ message, key: IFTTT_WEBHOOK_KEY }));
