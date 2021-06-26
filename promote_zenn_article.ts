642010ded72f0a8434b137cf9bd42275a6ea4bde
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-26 11:45:43 +0900

    fix promote_zenn_article to use api

--- promote_zenn_article.ts
+++ promote_zenn_article.ts
@@ -1,41 +1,32 @@
-import { DOMParser, ky } from "./deps.ts";
-const ZENN_ROOT = "https://zenn.dev";
+import { IFTTT_WEBHOOK_KEY } from "./env.ts";
+import { sendTweet } from "./tweet_with_ifttt.ts";
+import {
+  zennApi,
+  zennLink,
+  ZennTopic,
+} from "https://github.com/kawarimidoll/deno-zenn-api/raw/main/mod.ts";
 
-const genPromotion = async (username: string) => {
-  const dom = new DOMParser().parseFromString(
-    await ky(username, { prefixUrl: ZENN_ROOT }).text(),
-    "text/html",
-  );
-  if (!dom) {
-    console.error("Parse error");
-    Deno.exit(1);
-  }
+const { articles } = await zennApi("kawarimidoll");
+const article = articles[0];
+if (!article) {
+  throw new Error("No articles found");
+}
 
-  console.log("parse succeed");
-  const article = dom.getElementsByTagName("article")[0];
-  if (!article) {
-    console.error("Article not found");
-    Deno.exit(1);
-  }
+const { title, emoji, topics, readingTime } = article;
+const link = zennLink(article);
 
-  const title = article.getElementsByTagName("h3")[0].innerText;
-  const links = article.getElementsByTagName("a");
-  const link = ZENN_ROOT + links[1].getAttribute("href");
-  const tags = links.reduce((acc, current) => {
-    const path = current.getAttribute("href");
-    return (path?.startsWith("/topics")) ? [...acc, current.innerText] : acc;
-  }, [] as string[]);
-  const tagText = tags[0]
-    ? `${tags.join("とか") + (tags[1] ? "とか" : "")}についていろいろ書いています`
+const genTopicsText = (topics: ZennTopic[]) =>
+  topics[0]
+    ? topics.sort((a, b) => b.taggingsCount - a.taggingsCount).map((t) =>
+      t.displayName + "とか"
+    ).join("") + "についていろいろ書いています"
     : "";
-  const readTime = links[links.length - 1].getElementsByTagName("span")[0]
-    .innerText.replace(/^(\d+).*$/, "$1");
 
-  return `『${title}』という #Zenn 記事を書きました
-${tagText}
-${readTime}分くらいで読めるのでスキマ時間のお供にどうぞ
+const message = `『${emoji} ${title}』という #Zenn 記事を書きました
+${genTopicsText(topics)}
+${readingTime}分くらいで読めるのでスキマ時間のお供にどうぞ
 ${link}
 （本ツイートはDeno🦕で自動生成しています）`;
-};
 
-export { genPromotion };
+console.log(message);
+console.log(await sendTweet({ message, key: IFTTT_WEBHOOK_KEY }));
--- run_promote_zenn_article.ts
+++
@@ -1,7 +0,0 @@
-import { IFTTT_WEBHOOK_KEY } from "./env.ts";
-import { sendTweet } from "./tweet_with_ifttt.ts";
-import { genPromotion } from "./promote_zenn_article.ts";
-
-const message = await genPromotion("kawarimidoll");
-console.log({ message });
-console.log(await sendTweet({ message, key: IFTTT_WEBHOOK_KEY }));
