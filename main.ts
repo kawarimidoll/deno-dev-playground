2ddd749de6ba5c8600aab17863220b3606c6c6c9
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-27 09:31:51 +0900

    add promote_zenn_article to velociraptor.yml

--- main.ts
+++ main.ts
@@ -29,5 +29,7 @@
 //
 // console.log(feed);
 
-import { genPromotion } from "./promote_zenn_article.ts";
-console.log(await genPromotion("kawarimidoll"));
+import { zennApi } from "https://github.com/kawarimidoll/deno-zenn-api/raw/main/mod.ts";
+console.log(await zennApi("topics"));
+console.log(await zennApi("scraps/explore"));
+console.log(await zennApi("books", { order: "alltime", page: 2 }));
