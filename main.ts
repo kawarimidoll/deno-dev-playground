b0e78d03e84d10b3607962c0da462222a50235b4
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-28 17:55:56 +0900

    add graphql sample

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
-import { rss } from "./rss.ts";
+import { ky } from "./deps.ts";
+import { GITHUB_READ_USER_TOKEN } from "./env.ts";
 
-console.log(await rss("https://zenn.dev/kawarimidoll/feed"));
+const query = `
+query {
+  viewer {
+    login
+  }
+}
+`;
+
+const url = "https://api.github.com/graphql";
+
+const json = { query };
+
+const result = await ky.post(url, {
+  headers: { Authorization: `Bearer ${GITHUB_READ_USER_TOKEN}` },
+  json,
+}).json();
+
+console.log(result);
