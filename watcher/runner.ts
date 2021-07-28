32084102840f9eeaee980a152cb53e3dde8bb40b
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-28 14:20:50 +0900

    add watcher runner and sample

---
+++ watcher/runner.ts
@@ -0,0 +1,30 @@
+function denoRun(cmd: string[], currentProcess?: Deno.Process) {
+  if (currentProcess) {
+    currentProcess.close();
+  }
+
+  if (!cmd || !cmd[0]) {
+    throw new Error("Command inputs are required");
+  }
+  return Deno.run({ cmd: ["deno", ...cmd] });
+}
+
+async function watchProcessError(process: Deno.Process, onError: VoidFunction) {
+  try {
+    if ((await process.status()).success === false) {
+      onError();
+    }
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+export function runAndWatchErrors(
+  cmd: string[],
+  onError: VoidFunction,
+  ongoingProcess?: Deno.Process,
+) {
+  const process = denoRun(cmd, ongoingProcess);
+  watchProcessError(process, onError);
+  return process;
+}
---
+++ watcher/sample.ts
@@ -0,0 +1,5 @@
+console.log("My app is running...!");
+
+// setTimeout(() => {
+//   throw new Error("My app has thrown an error!");
+// }, 5000);
