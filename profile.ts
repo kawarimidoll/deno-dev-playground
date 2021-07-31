4bbb96e13dbf1d6cc47f6f20c1c169ffdda2847a
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-31 14:14:02 +0900

    improve profile

--- profile.ts
+++ profile.ts
@@ -1,4 +1,4 @@
-import { NBSP, tag as h } from "https://deno.land/x/markup_tag@0.1.2/mod.ts";
+import { tag as h } from "https://deno.land/x/markup_tag@0.1.2/mod.ts";
 
 const css = (cssObject: Record<string, Record<string, unknown>>) =>
   Object.entries(cssObject).map(([selector, attributes]) =>
@@ -9,26 +9,46 @@ const css = (cssObject: Record<string, Record<string, unknown>>) =>
 
 const title = "kawarimidoll";
 const styles = css({
-  body: { display: "flex", "justify-content": "center" },
-  "#main": { padding: "0.5rem 2rem" },
-  ".avatar": { "border-radius": "50%" },
+  body: { display: "flex", "justify-content": "center", margin: "0" },
+  a: { "text-decoration": "none" },
+  "h1,h2": { "text-align": "center" },
+  h2: { "margin-block-start": "2rem", "margin-block-end": "0" },
+  img: { display: "block", margin: "0 auto" },
+  "#main": { width: "100%", "max-width": "800px", padding: "1rem 0.5rem" },
+  ".avatar": { "border-radius": "50%", height: "auto" },
+  ".list-group": { "max-width": "500px", margin: "0 auto" },
   ".list-item": {
     "border-radius": "5px",
     "border-style": "solid",
     "border-width": "thin",
-    margin: "0.5rem",
+    margin: "0.5rem auto",
     padding: "0.5rem 2rem",
+  },
+  ".nav": {
+    display: "flex",
+    "justify-content": "space-around",
+    width: "60%",
+    margin: "0 auto",
+    "max-width": "500px",
+  },
+  ".nav>a": {
+    display: "block",
   },
 });
 
-const iconText = (icon: string, text: string, href?: string) =>
+const icongram = (name: string, size = 20) =>
+  h("img", { src: `https://icongr.am/${name}.svg?size=${size}`, alt: name });
+
+const iconText = (icon: string, text: string) =>
   h(
     "div",
     { class: "list-item" },
-    h("img", { src: `https://icongr.am/${icon}.svg?size=18` }),
-    NBSP,
-    href ? h("a", { href }, text) : text,
+    icongram(icon),
+    h("div", { style: "margin:0 auto;text-align:center" }, text),
   );
+
+const iconLink = (icon: string, text: string, href: string) =>
+  h("a", { href }, iconText(icon, text));
 
 const html = "<!DOCTYPE html>" +
   h(
@@ -36,6 +56,10 @@ const html = "<!DOCTYPE html>" +
     h(
       "head",
       h("meta", { charset: "utf-8" }),
+      h("meta", {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1.0",
+      }),
       h("title", title),
       h("style", styles),
       h("link", {
@@ -51,53 +75,68 @@ const html = "<!DOCTYPE html>" +
         "div",
         { id: "main" },
         h("img", {
-          style: "height:auto;display:block;margin:0 auto",
           alt: "avatar",
           width: "260",
-          height: "260",
           class: "avatar",
           src: "https://avatars.githubusercontent.com/u/8146876?v=4",
         }),
-        h("h1", { style: "text-align:center" }, "kawarimidoll"),
-        h("hr"),
-        h("h2", "I am..."),
-        iconText("feather/cpu", "Software developer"),
-        iconText("feather/scissors", "Yak shaver"),
-        iconText("feather/life-buoy", "Wheel reinventor"),
-        iconText("feather/triangle", "Indoor climber"),
-        iconText("feather/trending-up", "Long-term investor"),
-        iconText("feather/map-pin", "in the Room 101, Japan"),
-        h("hr"),
-        h("h2", "I am using..."),
-        iconText("feather/monitor", "macOS"),
-        iconText("feather/smartphone", "iPhone"),
-        iconText("feather/terminal", "Neovim"),
-        iconText("feather/heart", "Deno"),
-        h("hr"),
-        h("h2", "links"),
-        iconText(
-          "feather/twitter",
-          "Twitter",
-          "https://twitter.com/kawarimidoll",
+        h("h1", "kawarimidoll"),
+        h(
+          "div",
+          { class: "nav" },
+          h("a", { href: "#profiles" }, icongram("feather/smile", 26)),
+          h("a", { href: "#tools" }, icongram("feather/tool", 26)),
+          h("a", { href: "#links" }, icongram("feather/link", 26)),
         ),
-        iconText("feather/github", "GitHub", "https://github.com/kawarimidoll"),
-        iconText("feather/grid", "Pixela", "https://pixe.la/@kawarimidoll"),
-        iconText("feather/book", "Zenn", "https://zenn.dev/kawarimidoll"),
-        iconText("feather/search", "Qiita", "https://qiita.com/kawarimidoll"),
-        iconText(
-          "feather/coffee",
-          "Buy me a coffee",
-          "https://www.buymeacoffee.com/kawarimidoll",
-        ),
-        iconText(
-          "feather/gitlab",
-          "GitLab [stale]",
-          "https://gitlab.com/kawarimidoll",
-        ),
-        iconText(
-          "feather/package",
-          "npm [stale]",
-          "https://www.npmjs.com/~kawarimidoll",
+        h("hr"),
+        h(
+          "div",
+          { class: "list-group" },
+          h("h2", { id: "profiles" }, icongram("feather/smile", 40)),
+          iconText("feather/cpu", "Software developer"),
+          iconText("feather/scissors", "Yak shaver"),
+          iconText("feather/life-buoy", "Wheel reinventor"),
+          iconText("feather/triangle", "Indoor climber"),
+          iconText("feather/trending-up", "Long-term investor"),
+          iconText("feather/map-pin", "Room 101, Japan"),
+          h("h2", { id: "tools" }, icongram("feather/tool", 40)),
+          iconText("feather/monitor", "macOS"),
+          iconText("feather/smartphone", "iPhone"),
+          iconText("feather/terminal", "Neovim"),
+          iconText("feather/heart", "Deno"),
+          h("h2", { id: "links" }, icongram("feather/link", 40)),
+          iconLink(
+            "feather/twitter",
+            "Twitter",
+            "https://twitter.com/kawarimidoll",
+          ),
+          iconLink(
+            "feather/github",
+            "GitHub",
+            "https://github.com/kawarimidoll",
+          ),
+          iconLink("feather/grid", "Pixela", "https://pixe.la/@kawarimidoll"),
+          iconLink("feather/book", "Zenn", "https://zenn.dev/kawarimidoll"),
+          iconLink(
+            "feather/search",
+            "Qiita",
+            "https://qiita.com/kawarimidoll",
+          ),
+          iconLink(
+            "feather/coffee",
+            "Buy me a coffee",
+            "https://www.buymeacoffee.com/kawarimidoll",
+          ),
+          iconLink(
+            "feather/gitlab",
+            "GitLab [stale]",
+            "https://gitlab.com/kawarimidoll",
+          ),
+          iconLink(
+            "feather/package",
+            "npm [stale]",
+            "https://www.npmjs.com/~kawarimidoll",
+          ),
         ),
       ),
     ),
