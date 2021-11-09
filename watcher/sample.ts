5c974cca447953a26ef3cbf041accbc34308feed
kawarimidoll <kawarimidoll+git@gmail.com>
2021-11-09 10:07:52 +0900

    update sample scripts

--- watcher/sample.ts
+++ watcher/sample.ts
@@ -1,5 +1,13 @@
+import { name } from "./const.ts";
 console.log("My app is running...!");
+console.log("hello", name);
+
+for (let i = 1; i < 11; i++) {
+  setTimeout(() => {
+    console.log(`${i}...`);
+  }, i * 1000);
+}
 
 // setTimeout(() => {
 //   throw new Error("My app has thrown an error!");
-// }, 5000);
+// }, 1000);
